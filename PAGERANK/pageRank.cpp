f899ce40200569a4ab81b3567b6a653594c08c81
minhaj-shakeel <cs1160354@iitd.ac.in>
2020-04-16 14:12:19 +0530

    handling dangling pages

--- PAGERANK/pageRank.cpp
+++ PAGERANK/pageRank.cpp
@@ -16,26 +16,28 @@
 #include <fstream>
 #include <utility>
 #include <numeric>
-
+#include <unordered_map>
 
 
 namespace pageRank{
 
-  std::vector<std::vector<double> > Edges ;
-  std::vector<double> rank;
-
+  std::vector<std::vector<int> > Edges ;
+  std::unordered_map<int,std::vector<int> > LinkMap ;
+  std::vector<double> initRank,newRank;
+  int n;
+  double alpha;
   template<typename  MapTask>
   class datasource : mapreduce::detail::noncopyable
   {
     public:
-      datasource(long size) : sequence_(1),size_(size) 
+      datasource(long size) : sequence_(0),size_(size) 
      {
      }
      bool const setup_key(typename MapTask::key_type &key)
      {
        key = sequence_++; //pages are numbered from 1 to n
 
-       return key <= size_;
+       return key < size_;
      }
 
      bool const get_data(typename MapTask::key_type const &key , typename MapTask::value_type &value)
@@ -50,8 +52,7 @@ namespace pageRank{
                                     its pageRank is .10 
                                     and it links to 3 pages whose ids are 2,4,5
        */
-       value.first = rank[key-1];
-       value.second = Edges[key-1];
+       value = initRank[key];
        return true;
      }
     private:
@@ -61,7 +62,7 @@ namespace pageRank{
 
 
 
-  struct map_task : public mapreduce::map_task<double, std::pair<double , std::vector<double> > >
+  struct map_task : public mapreduce::map_task< int, double >
   {
     template<typename Runtime>
     void operator()(Runtime &runtime,key_type const &key , value_type const &value) const
@@ -70,13 +71,30 @@ namespace pageRank{
       /*Mapper returns the pageId and corresponding increase 
        in its pageRank because of page whose pageId= key links to it*/
       runtime.emit_intermediate(key,0);
-      for(int i = 1 ; i <= value.second[0]; i++){
-        runtime.emit_intermediate(value.second[i],value.first/value.second[0]);
+      int nLinks = LinkMap[key].size();
+      std::vector<int> LinkVector = LinkMap[key];
+      for(int i= 0  ; i < nLinks; i++){
+       // std::cout <<  "key " << key << " " << LinkVector[i] << " " << value/nLinks << std::endl;
+        runtime.emit_intermediate(LinkVector[i],value/nLinks);
       }
     }
   };                 
   
-  struct reduce_task : public mapreduce::reduce_task<double , double >
+  struct danglingmap_task : public mapreduce::map_task< int, double >
+  {
+    template<typename Runtime>
+    void operator()(Runtime &runtime,key_type const &key , value_type const &value) const
+    {
+
+      int dkey = 0 ;
+      runtime.emit_intermediate(dkey,0); //for safety purpose
+
+      int nLinks = LinkMap[key].size();
+      if (nLinks == 0)
+        runtime.emit_intermediate(dkey,value);
+    }
+  };                 
+  struct reduce_task : public mapreduce::reduce_task<int, double >
   {  
     template<typename Runtime , typename It>
     void operator()(Runtime &runtime , key_type const &key , It it , It const  ite) const
@@ -98,7 +116,14 @@ namespace pageRank{
                  pageRank::reduce_task,
                  mapreduce::null_combiner,
                  pageRank::datasource<pageRank::map_task>
-  >job;
+  >job1;
+
+  typedef 
+  mapreduce::job<pageRank::danglingmap_task,
+                 pageRank::reduce_task,
+                 mapreduce::null_combiner,
+                 pageRank::datasource<pageRank::map_task>
+  >job2;
 
 } //namespace pageRank
 
@@ -115,6 +140,17 @@ bool converge(std::vector<double> initRank , std::vector<double> newRank , doubl
       
 }
 
+void writeToFile(std::string filename){
+  std::ofstream outfile;
+  outfile.open(filename);
+  double sum =0 ;
+  for(int i = 0 ; i < pageRank::newRank.size() ; i++){
+    sum+=pageRank::newRank[i];
+    outfile << pageRank::newRank[i] << std::endl;
+  }
+  outfile << "sum= " << sum << std::endl;
+  outfile.close();
+}
 
 int main(int argc , char **argv){
   /*taking input from a file in which first 2 integers represent 
@@ -128,55 +164,73 @@ int main(int argc , char **argv){
   std::ifstream inFile ;
   inFile.open(argv[1]);
   int n,links;
-  inFile >> n;
-  inFile >> links;
-
+  n=0;
   //Pages are numbered from 1 to n and not from 0 to n-1
-  std::vector<std::vector<double> > Edgelist(n,std::vector<double>(1,0));
-  for(int i = 0 ; i < links ; i++){
-      int a,b;
-      inFile >> a >>  b ;
-      Edgelist[a-1][0]++;
-      Edgelist[a-1].push_back(b);
+  int a,b;
+
+  while(inFile >> a >> b ){
+      a--;b--;
+      n=std::max(b,std::max(a,n));
+      pageRank:: LinkMap[a].push_back(b);
   }
+  n++;
   
+  pageRank::alpha = .85;
+
   /*Initilial pageRanks are stored in initRank and newly computed in newRank*/
-  std::vector<double> initRank(n,0);
-  std::vector<double> newRank(n,1.0/n);
+  pageRank::initRank= std::vector<double>(n,0);
+  pageRank::newRank= std::vector<double>(n,1.0/n);
 
   //Storing the Edges 
-  pageRank::Edges = Edgelist;
 
   int iteration = 0; //to store the number of count till convergence
-  while(!converge(initRank,newRank,.0001)){
+  while(!converge(pageRank::initRank,pageRank::newRank,.0001)){
      iteration++;
-     initRank = newRank;
-     pageRank::rank = initRank;
+     pageRank::initRank = pageRank::newRank;
      mapreduce::specification spec;
-     pageRank::job::datasource_type datasource(8);
+     pageRank::job1::datasource_type datasource1(n);
+     pageRank::job2::datasource_type datasource2(n);
      spec.map_tasks = 1;
      spec.reduce_tasks=std::max(1U,std::thread::hardware_concurrency());
 
-     pageRank::job job(datasource,spec);
-     
-     mapreduce::results result;
+     pageRank::job1 job1(datasource1,spec);
+     pageRank::job2 job2(datasource2,spec);
+
+     mapreduce::results result1;
+     mapreduce::results result2;
+  
    # ifdef _DEBUG 
-     job.run<mapreduce::schedule_policy::sequential<pageRank::job> >(result);
+     job1.run<mapreduce::schedule_policy::sequential<pageRank::job1> >(result1);
    # else 
-     job.run<mapreduce::schedule_policy::cpu_parallel<pageRank::job> >(result);
+     job1.run<mapreduce::schedule_policy::cpu_parallel<pageRank::job1> >(result1);
    # endif
      
-     for(auto it=job.begin_results();it!=job.end_results();++it)
+     for(auto it=job1.begin_results();it!=job1.end_results();++it)
      {
-       newRank[it->first-1]=it->second;
+       pageRank::newRank[it->first]=pageRank::alpha*(it->second);
      }
-  }
+     
+   # ifdef _DEBUG 
+     job2.run<mapreduce::schedule_policy::sequential<pageRank::job2> >(result2);
+   # else 
+     job2.run<mapreduce::schedule_policy::cpu_parallel<pageRank::job2> >(result2);
+   # endif
+   
 
+     for(auto it=job2.begin_results();it!=job2.end_results();++it)
+     {  
+        for(int i = 0 ; i < n ; i ++){
+          pageRank::newRank[i]+= (pageRank::alpha*(it->second) + 1-pageRank::alpha)/n ;
+        }
+  
+     }
+
+  }
   std::cout << "Number of iterations " << iteration << std::endl;
   
   for(int i = 0 ; i < n ; i++){
-    std::cout << "Page " <<  i+1 << " " <<  newRank[i] << std::endl;
+    std::cout << "Page " <<  i+1 << " " <<  pageRank::newRank[i] << std::endl;
   }
-  
+  writeToFile(argv[2]); 
 }
 
